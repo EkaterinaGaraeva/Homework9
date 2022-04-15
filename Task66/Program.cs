47b4effc9ab253410c8862cfa0ffdb6e94595f5c
Ваше имя <garaevaea@mail.ru>
2022-04-15 04:43:35 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumbersFromMToN(int numberM, int numberN, int sum = 0)
+{
+    if (numberM <= numberN)
+    {
+        sum += numberM;
+        sum = SumNumbersFromMToN(++numberM, numberN, sum);
+    }
+    return sum;
+}
+
+int numM = 4;
+int numN = 8;
+int sumNumbers = SumNumbersFromMToN(numM, numN);
+Console.WriteLine($"Сумма чисел от {numM} до {numN} = {sumNumbers}");

90bf2cdf431b50025d9e015532be1a9fca60edb0
Ваше имя <garaevaea@mail.ru>
2022-04-15 04:33:53 +0300

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до M.
+M = 1; N = 5. -> "5, 4, 3, 2, 1"
+M = 4; N = 8. -> "8, 7, 6, 7"
+*/
+
+void NaturalNumbersFromNToM(int numberM, int numberN)
+{
+    if (numberN < numberM)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write($"{numberN}");
+        if (numberN > numberM)
+        {
+            Console.Write($", ");
+        }
+        NaturalNumbersFromNToM(numberM, --numberN);
+    }
+}
+
+NaturalNumbersFromNToM(1, 5);
